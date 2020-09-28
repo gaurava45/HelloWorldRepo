62495ac692a752f193556d1b47aa307fca6e7b2d
gaurava45 <70994685+gaurava45@users.noreply.github.com>
2020-09-28 16:02:17 +0530

    Update HelloWorld.java

--- TestProject/src/HelloWorld.java
+++ TestProject/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World for test repository");
+System.out.println("Hello World for test project for hello world repository");
 	}
 
 }
